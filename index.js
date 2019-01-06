8b1066f138880401e783988dbd46a41ad1c0fc62
mwestervelt <elle.westervelt@gmail.com>
2019-01-06 12:50:41 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your code in this file!
+function scuberGreetingForFeet(value) {
+  if (value <= 400) {
+    return 'This one is on me!';
+} else if (2000 <= value && value <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+} else  {
+    return 'No can do.';
+  }
+}
+
+function ternaryCheckCity(city) {
+  if (city === 'NYC') {
+    return 'Ok, sounds good.';
+  } else {
+    return 'No go.';
+  }
+}
+
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default: ''
+      return 'Bye.';
+  }
+}
