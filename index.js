edb7dd7517ff804c9a2b4417c141da7e0cf593ca
jonbiro <jonnybrx@gmail.com>
2019-01-06 18:30:21 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your code in this file!
+function scuberGreetingForFeet (a) {
+  if (a > 2500) {
+    return 'No can do.'
+  } else if (a <= 400) {
+    return 'This one is on me!'
+  } else if (a > 2000) {
+  return  'I will gladly take your thirty bucks.'
+  }
+return 't'
+
+}
+
+function ternaryCheckCity(city){
+  if (city == "NYC"){
+    return 'Ok, sounds good.'
+  }{
+    return "No go."
+  }
+
+}
+function switchOnCharmFromTip(tip){
+  if (tip == 'generous') {
+    return "Thank you so much."
+
+  } else {
+    if (tip == 'not as generous'){
+      return 'Thank you.'
+    }{
+      return 'Bye.'
+    }
+  }
+
+
+
+
+}
